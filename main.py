92232acb3e750d2e7b279763fdbb3a6ba7f569f1
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2023-12-16 21:10:23 +1000

    Чтение данных

---
+++ main.py
@@ -0,0 +1,4 @@
+from read_data import read_data
+
+data_x, data_y = read_data(["Age", "HR", "Killip class", "Cr", "EF LV", "NEUT","EOS", "PCT", "Glu", "SBP"], "Dead")
+
--- read_data.py
+++ read_data.py
@@ -1,2 +1,18 @@
 # Источник данных: https://github.com/NikitaKuksin/PublicCodAticle
 
+import pandas as pd
+
+# Принимает:
+#   predictors - список имен входных признаков
+#   output - имя выходного признака
+# Возвращает:
+#   x - двумерный массив значений входных признаков
+#   y - одномерный массив значений выходного признака
+def read_data(predictors, output):
+    path_dataset = r'DataSet.xlsx'
+    dataset = pd.read_excel(path_dataset)
+    dataset = dataset[predictors + [output]].dropna()
+    x = dataset[predictors].to_numpy()
+    y = dataset[output].to_numpy()
+    return x, y
+

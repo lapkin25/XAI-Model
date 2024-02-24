50d6d0244a485deea1b6c98473e76e77026db46e
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2024-02-24 17:33:50 +1000

    Cross-validation and final testing

--- dichotomization/main.py
+++ dichotomization/main.py
@@ -3,6 +3,7 @@ from sklearn.linear_model import LogisticRegression
 from initial_model import InitialModel
 import numpy as np
 from sklearn import metrics as sklearn_metrics
+from sklearn.model_selection import StratifiedKFold, train_test_split
 
 
 def find_predictors_to_invert(data, predictors):
@@ -28,7 +29,7 @@ data.prepare(predictors, "Dead", invert_predictors)
 threshold = 0.05
 
 initial_model = InitialModel()
-# TODO: кросс-валидация и итоговое тестирование
+"""
 initial_model.fit(data.x, data.y)
 print("Начальная модель")
 #print("Пороги:", initial_model.cutoffs)
@@ -43,4 +44,26 @@ print("Интерсепт:", initial_model.intercept)
 p = initial_model.predict_proba(data.x, data.y)
 y_pred = np.where(p >= 0.05, 1, 0)
 auc = sklearn_metrics.roc_auc_score(data.y, p)
+print("AUC:", auc)
+"""
+
+print("Кросс-валидация")
+skf = StratifiedKFold(n_splits=8, shuffle=True, random_state=123)
+for i, (train_index, test_index) in enumerate(skf.split(data.x, data.y)):
+    print(f"Fold {i}:")
+    x_train = data.x[train_index, :]
+    y_train = data.y[train_index]
+    x_test = data.x[test_index, :]
+    y_test = data.y[test_index]
+    initial_model.fit(x_train, y_train)
+    p = initial_model.predict_proba(x_test, y_test)
+    auc = sklearn_metrics.roc_auc_score(y_test, p)
+    print("AUC:", auc)
+
+print("Итоговое тестирование")
+x_train, x_test, y_train, y_test =\
+    train_test_split(data.x, data.y, test_size=0.2, random_state=123, stratify=data.y)
+initial_model.fit(x_train, y_train)
+p = initial_model.predict_proba(x_test, y_test)
+auc = sklearn_metrics.roc_auc_score(y_test, p)
 print("AUC:", auc)

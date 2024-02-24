47c690c3a24f188f085f823140effff7e13a2684
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2024-02-24 15:17:38 +1000

    Indicating the features to invert

--- dichotomization/main.py
+++ dichotomization/main.py
@@ -1,14 +1,23 @@
 from read_data import Data
+from sklearn.linear_model import LogisticRegression
+
 
 def find_predictors_to_invert(data, predictors):
-    # TODO: обучить логистическую регрессию с выделенными признаками,
-    #   выбрать признаки с отрицательными весами
-    pass
+    # обучаем логистическую регрессию с выделенными признаками,
+    #   выбираем признаки с отрицательными весами
+    data.prepare(predictors, "Dead", [])
+    logist_reg = LogisticRegression()
+    logist_reg.fit(data.x, data.y)
+    weights = logist_reg.coef_.ravel()
+    invert_predictors = []
+    for i, feature_name in enumerate(predictors):
+        if weights[i] < 0:
+            invert_predictors.append(feature_name)
+    return invert_predictors
 
 
 data = Data("DataSet.xlsx")
 predictors = ["Age", "HR", "Killip class", "Cr", "EF LV", "NEUT", "EOS", "PCT", "Glu", "SBP"]
-invert_predictors = []
+invert_predictors = find_predictors_to_invert(data, predictors)
+print("Признаки с отрицательными весами:", invert_predictors)
 data.prepare(predictors, "Dead", invert_predictors)
-#print(data.x)
-#print(data.y)

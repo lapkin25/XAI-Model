3e652d5fbb56f6b2ab4e98c0cbef511976263152
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2024-03-01 06:25:48 +1000

    Some refactoring

--- dichotomization/adjusted_model.py
+++ dichotomization/adjusted_model.py
@@ -45,49 +45,53 @@ class AdjustedModel:
         # TODO: передать порог (сейчас 5%) в качестве входного параметра
         for it in range(num_iter):
             print("Iteration", it + 1)
-            for k in np.random.permutation(num_features):  #range(num_features):
-                # производим дихотомизацию
-                bin_x = self.dichotomize(x)
-                # исключаем k-й признак
-                weights1 = np.delete(self.weights, k)
-                bin_x1 = np.delete(bin_x, k, axis=1)
-                intercept1 = AdjustIntercept(weights1, self.intercept).fit(bin_x1, y)
-                # значения решающей функции для каждой точки
-                logit = np.array([intercept1 + np.dot(weights1, bin_x1[i])
-                                  for i in range(data_size)])
-                # выделяем пороговую область
-                selection = logit < logit_threshold
-                logit1 = logit[selection]
-                xk = x[selection, k]
-                labels = y[selection]
-                # находим пороги, обеспечивающие максимум TPV/FPV
-                xk_cutoff, min_logit, max_rel = max_ones_zeros(xk, logit1, labels, 10)
-                #xk_cutoff, min_logit, max_rel = eps_max_ones_zeros_min_y(xk, logit1, labels, 10, eps=8.0)
-                # обновляем порог и вес для k-го признака
-                self.cutoffs[k] = xk_cutoff
-                self.weights[k] = logit_threshold - min_logit
-                # интерсепт пока не трогаем, потому что
-                #   для следующего признака он настраивается заново
-            # настраиваем интерсепт в конце каждой итерации
-            bin_x = self.dichotomize(x)
-            self.intercept = AdjustIntercept(self.weights, self.intercept).fit(bin_x, y)
-            if verbose:
-                print("Пороги:", self.cutoffs)
-                print("Веса:", self.weights)
-                print("Интерсепт:", self.intercept)
+            self.make_iteration(x, y, logit_threshold, verbose)
 
-            # настраиваем веса и интерсепт, обучая логистическую регрессию
-            self.fit_logistic(x, y)
-            
-            if verbose:
-                print("После обучения логистической регрессии:")
-                print("Веса:", self.weights)
-                print("Интерсепт:", self.intercept)
-            # вычисляем AUC
-            probs = self.predict_proba(x, y)
-            auc = sklearn_metrics.roc_auc_score(y, probs)
-            if verbose:
-                print("AUC:", auc)
+    def make_iteration(self, x, y, logit_threshold, verbose):
+        data_size, num_features = x.shape[0], x.shape[1]
+        for k in np.random.permutation(num_features):  # range(num_features):
+            # производим дихотомизацию
+            bin_x = self.dichotomize(x)
+            # исключаем k-й признак
+            weights1 = np.delete(self.weights, k)
+            bin_x1 = np.delete(bin_x, k, axis=1)
+            intercept1 = AdjustIntercept(weights1, self.intercept).fit(bin_x1, y)
+            # значения решающей функции для каждой точки
+            logit = np.array([intercept1 + np.dot(weights1, bin_x1[i])
+                              for i in range(data_size)])
+            # выделяем пороговую область
+            selection = logit < logit_threshold
+            logit1 = logit[selection]
+            xk = x[selection, k]
+            labels = y[selection]
+            # находим пороги, обеспечивающие максимум TPV/FPV
+            xk_cutoff, min_logit, max_rel = max_ones_zeros(xk, logit1, labels, 10)
+            # xk_cutoff, min_logit, max_rel = eps_max_ones_zeros_min_y(xk, logit1, labels, 10, eps=8.0)
+            # обновляем порог и вес для k-го признака
+            self.cutoffs[k] = xk_cutoff
+            self.weights[k] = logit_threshold - min_logit
+            # интерсепт пока не трогаем, потому что
+            #   для следующего признака он настраивается заново
+        # настраиваем интерсепт в конце каждой итерации
+        bin_x = self.dichotomize(x)
+        self.intercept = AdjustIntercept(self.weights, self.intercept).fit(bin_x, y)
+        if verbose:
+            print("Пороги:", self.cutoffs)
+            print("Веса:", self.weights)
+            print("Интерсепт:", self.intercept)
+
+        # настраиваем веса и интерсепт, обучая логистическую регрессию
+        self.fit_logistic(x, y)
+
+        if verbose:
+            print("После обучения логистической регрессии:")
+            print("Веса:", self.weights)
+            print("Интерсепт:", self.intercept)
+        # вычисляем AUC
+        probs = self.predict_proba(x, y)
+        auc = sklearn_metrics.roc_auc_score(y, probs)
+        if verbose:
+            print("AUC:", auc)
 
     # взять веса и интерсепт из логистической регрессии при заданных порогах
     def fit_logistic(self, x, y):

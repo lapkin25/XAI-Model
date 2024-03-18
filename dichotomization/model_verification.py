0927b52e178107e9d9f8ebe297bf3c3d2b49be11
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2024-03-19 08:09:48 +1000

    Model verification

---
+++ dichotomization/model_verification.py
@@ -0,0 +1,77 @@
+import numpy as np
+from read_data import Data
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics as sklearn_metrics
+from sklearn.model_selection import StratifiedKFold, train_test_split
+
+
+def test_model(model, data_x, data_y, p_threshold=0.05):
+    print("Кросс-валидация")
+    skf = StratifiedKFold(n_splits=7)
+    for i, (train_index, test_index) in enumerate(skf.split(data_x, data_y)):
+        print(f"Fold {i}:")
+        x_train = data_x[train_index, :]
+        y_train = data_y[train_index]
+        x_test = data_x[test_index, :]
+        y_test = data_y[test_index]
+        model.fit(x_train, y_train)
+        p = model.predict_proba(x_test)[:, 1]
+        auc = sklearn_metrics.roc_auc_score(y_test, p)
+        print("AUC:", auc)
+        # выводим качество модели
+        y_pred = np.where(p > p_threshold, 1, 0)
+        tn, fp, fn, tp = sklearn_metrics.confusion_matrix(y_test, y_pred).ravel()
+        specificity = tn / (tn + fp)
+        sensitivity = tp / (tp + fn)
+        print("Sens:", sensitivity, "Spec:", specificity)
+        print("tp =", tp, "fn =", fn, "fp =", fp, "tn =", tn)
+
+    print("Итоговое тестирование")
+    x_train, x_test, y_train, y_test = \
+        train_test_split(data_x, data_y, test_size=0.2, random_state=123, stratify=data_y)
+    model.fit(x_train, y_train)
+    p = model.predict_proba(x_test)[:, 1]
+    auc = sklearn_metrics.roc_auc_score(y_test, p)
+    print("AUC:", auc)
+    # выводим качество модели
+    y_pred = np.where(p > p_threshold, 1, 0)
+    tn, fp, fn, tp = sklearn_metrics.confusion_matrix(y_test, y_pred).ravel()
+    specificity = tn / (tn + fp)
+    sensitivity = tp / (tp + fn)
+    print("Веса:", model.coef_.ravel())
+    print("Sens:", sensitivity, "Spec:", specificity)
+    print("tp =", tp, "fn =", fn, "fp =", fp, "tn =", tn)
+
+
+data = Data("DataSet.xlsx")
+predictors = ["Cr", "HR", "EF LV", "Glu", "NEUT", "EOS", "PCT", "SBP", "Age", "Killip class"]
+thresholds = [229.44, 60.0, 30.0, 6.19, 89.2, 0.5, 0.32, 115.0, 68.0, 4.0]
+#invert_predictors = [False, False, True, False, False, True, False, True, False, False]
+invert_predictors = ["EF LV", "EOS", "SBP"]
+
+combined_predictors = [("Age", "EOS"), ("HR", "Age"), ("HR", "Glu"), ("Killip class", "NEUT"),
+                       ("Glu", "EF LV"), ("Glu", "Age"), ("Age", "NEUT"), ("SBP", "EOS"),
+                       ("Glu", "SBP"), ("Killip class", "HR"), ("EF LV", "Cr"),
+                       ("Killip class", "EF LV"), ("NEUT", "EF LV"), ("SBP", "HR"), ("EOS", "Age")]
+combined_thresholds = [0.0, 76.0, 8.98, 75.0, 40.0, 68.0, 76.6, 0.0, 90.0, 90.0, 105.61, 46.0, 54.62, 90.0, 64.0]
+
+bin_x = data.binarize(predictors, "Dead", invert_predictors, thresholds)
+bin_x_combined = data.binarize_combined(predictors, "Dead", invert_predictors, thresholds,
+                                        combined_predictors, combined_thresholds)
+
+data.prepare(predictors, "Dead", invert_predictors)
+
+# модель с непрерывными признаками
+print("Модель 1")
+model1 = LogisticRegression()
+test_model(model1, data.x, data.y)
+
+# модель с бинаризованными индивидуальными признаками
+print("Модель 2")
+model2 = LogisticRegression()
+test_model(model2, bin_x, data.y)
+
+# модель с бинаризованными комбинированными признаками
+print("Модель 3")
+model3 = LogisticRegression()
+test_model(model3, bin_x_combined, data.y)
--- dichotomization/read_data.py
+++ dichotomization/read_data.py
@@ -1,5 +1,6 @@
 # Источник данных: https://github.com/NikitaKuksin/PublicCodAticle
 
+import numpy as np
 import pandas as pd
 from sklearn import preprocessing
 
@@ -46,3 +47,40 @@ class Data:
         if feature_name in self.inverted_predictors:
             val = -val
         return val
+
+    def binarize(self, selected_predictors, output_feature, invert_predictors, thresholds):
+        data_x_y = self.dataset[selected_predictors + [output_feature]].dropna()
+        data_x = data_x_y[selected_predictors].to_numpy()
+        data_y = data_x_y[output_feature].to_numpy()
+        bin_x = np.empty_like(data_x)
+        for i, pred in enumerate(selected_predictors):
+            if pred in invert_predictors:
+                bin_x[:, i] = np.where(data_x[:, i] <= thresholds[i], 1, 0)
+            else:
+                bin_x[:, i] = np.where(data_x[:, i] >= thresholds[i], 1, 0)
+        return bin_x
+
+    def binarize_combined(self, selected_predictors, output_feature, invert_predictors, thresholds,
+                          combined_predictors, combined_thresholds):
+        data_x_y = self.dataset[selected_predictors + [output_feature]].dropna()
+        data_x = data_x_y[selected_predictors].to_numpy()
+        data_y = data_x_y[output_feature].to_numpy()
+        data_size = data_x.shape[0]
+        bin_x = np.zeros((data_size, len(combined_predictors)))
+        for i, (pred1, pred2) in enumerate(combined_predictors):
+            j = selected_predictors.index(pred1)
+            k = selected_predictors.index(pred2)
+            for s in range(data_size):
+                if pred1 in invert_predictors:
+                    bin_j = (data_x[s, j] <= thresholds[j])
+                else:
+                    bin_j = (data_x[s, j] >= thresholds[j])
+                if pred2 in invert_predictors:
+                    bin_k = (data_x[s, k] <= combined_thresholds[i])
+                else:
+                    bin_k = (data_x[s, k] >= combined_thresholds[i])
+                if bin_j and bin_k:
+                    bin_x[s, i] = 1
+                else:
+                    bin_x[s, i] = 0
+        return bin_x

e7ebb0bf71c17326b0b98f44f8b1c7c77a18731c
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2024-03-20 09:08:57 +1000

    New model

--- new_model/new_combined_features_model.py
+++ new_model/new_combined_features_model.py
@@ -1,5 +1,9 @@
 class NewCombinedFeaturesModel:
-    def __init__(self, verbose_training=False, p0=0.05, K=10):
+    # p0 - порог отсечения
+    # K - число комбинированных признаков
+    # delta_a - параметр регуляризации для порога
+    # delta_w - параметр регуляризации для веса
+    def __init__(self, verbose_training=False, p0=0.05, K=10, delta_a=None, delta_w=None):
         self.cutoffs = None
         self.weights = None
         self.intercept = None
@@ -9,9 +13,12 @@ class NewCombinedFeaturesModel:
         self.verbose_training = verbose_training
         self.p0 = p0
         self.K = K
+        self.delta_a = delta_a
+        self.delta_w = delta_w
 
 
     def fit(self, x, y):
+
 
 
     # возвращает для каждой точки две вероятности: "0" и "1"
---
+++ new_model/new_tpv_fpv.py
@@ -0,0 +1,48 @@
+import numpy as np
+from sortedcontainers import SortedList
+
+
+# Находит пороги a, b
+#   (в окрестности порогов a0, b0: |a - a0| <= da, |b - b0| <= db),
+#   максимизирующие отношение числа единиц к числу нулей
+#   в области {x >= a & y >= b}
+#   при условии, что число нулей в этой области не меньше min_zero_count
+# Возвращает числа a, b, max_rel
+#   a, b - пороги для x и y
+#   max_rel - максимум отношения числа единиц к числу нулей
+def new_max_ones_zeros(x_, y_, labels_, min_zero_count, a0, b0, da, db):
+    # сортируем точки по убыванию y
+    ind = np.argsort(y_)
+    ind = ind[::-1]
+    x = x_[ind]
+    y = y_[ind]
+    labels = labels_[ind]
+    n = x.size
+
+    max_rel = None
+    a = None
+    b = None
+
+    x_coord = SortedList()  # x-координаты всех добавленных точек
+    ones_x_coord = SortedList()  # x-координаты всех добавленных единиц
+    for i in range(n):
+        x_coord.add(x[i])
+        if labels[i] == 1:
+            ones_x_coord.add(x[i])
+            for j, x1 in enumerate(reversed(ones_x_coord)):
+                # ones_right - число единиц не левее x1
+                ones_right = j + 1
+                # points_right - число точек не левее x1
+                points_right = (i + 1) - x_coord.index(x1)
+                # zeros_right - число нулей не левее x1
+                zeros_right = points_right - ones_right
+                if zeros_right >= min_zero_count and\
+                        (a is None and b is None or\
+                        abs(x1 - a0) <= da and abs(y[i] - b0) <= db):
+                    rel = ones_right / zeros_right
+                    if max_rel is None or rel > max_rel:
+                        max_rel = rel
+                        a = x1
+                        b = y[i]
+
+    return a, b, max_rel

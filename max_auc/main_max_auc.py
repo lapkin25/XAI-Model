85debdacf584e2c35b6f20ce9186d055dd8bb904
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2024-05-18 23:33:06 +1000

    MaxAUC (start)

---
+++ max_auc/main_max_auc.py
@@ -0,0 +1,139 @@
+import sys
+sys.path.insert(1, '../dichotomization')
+
+from dichotomization.read_data import Data
+from max_auc_model import InitialMaxAUCModel, IndividualMaxAUCModel # , CombinedMaxAUCModel
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn import metrics as sklearn_metrics
+from permetrics.classification import ClassificationMetric
+
+def find_predictors_to_invert(data, predictors):
+    # обучаем логистическую регрессию с выделенными признаками,
+    #   выбираем признаки с отрицательными весами
+    data.prepare(predictors, "Dead", [])
+    logist_reg = LogisticRegression()
+    logist_reg.fit(data.x, data.y)
+    weights = logist_reg.coef_.ravel()
+    invert_predictors = []
+    for i, feature_name in enumerate(predictors):
+        if weights[i] < 0:
+            invert_predictors.append(feature_name)
+    return invert_predictors
+
+
+def print_model(model, data):
+    print("=" * 10 + "\nМодель")
+    print("Пороги:")
+    for k, feature_name in enumerate(predictors):
+        val = data.get_coord(feature_name, model.cutoffs[k])
+        s = '≤' if feature_name in data.inverted_predictors else '≥'
+        print(feature_name, " ", s, val, sep='')
+    """
+    print("Комбинированные пороги:")
+    for k, j, xj_cutoff in model.combined_features:
+        feature_name = predictors[k]
+        val = data.get_coord(feature_name, model.cutoffs[k])
+        s = '≤' if feature_name in data.inverted_predictors else '≥'
+        print(feature_name, " ", s, val, sep='', end='')
+        feature_name = predictors[j]
+        val = data.get_coord(feature_name, xj_cutoff)
+        s = '≤' if feature_name in data.inverted_predictors else '≥'
+        print(" & ", feature_name, " ", s, val, sep='')
+        tp = 0
+        fp = 0
+        y_pred = np.zeros(data.x.shape[0])
+        for i in range(data.x.shape[0]):
+            if data.x[i, k] >= model.cutoffs[k] and data.x[i, j] >= xj_cutoff:
+                y_pred[i] = 1
+                if data.y[i] == 1:
+                    tp += 1
+                else:
+                    fp += 1
+        print("   TP = ", tp, " FP = ", fp)
+        cm = ClassificationMetric(data.y, y_pred)
+        print("Gini = ", cm.gini_index(average=None))
+    print("Веса:", model.individual_weights)
+    print("Комбинированные веса:", model.combined_weights)
+    print("Интерсепт:", model.intercept)
+    """
+
+def test_model(model, x_test, y_test, p_threshold):
+    p = model.predict_proba(x_test)[:, 1]
+    auc = sklearn_metrics.roc_auc_score(y_test, p)
+    print("AUC:", auc)
+    # выводим качество модели
+    y_pred = np.where(p > p_threshold, 1, 0)
+    tn, fp, fn, tp = sklearn_metrics.confusion_matrix(y_test, y_pred).ravel()
+    specificity = tn / (tn + fp)
+    sensitivity = tp / (tp + fn)
+    print("Sens:", sensitivity, "Spec:", specificity)
+    print("tp =", tp, "fn =", fn, "fp =", fp, "tn =", tn)
+    return auc, sensitivity, specificity
+
+
+data = Data("DataSet.xlsx")
+predictors = ["Age", "HR", "Killip class", "Cr", "EF LV", "NEUT", "EOS", "PCT", "Glu", "SBP"]
+invert_predictors = find_predictors_to_invert(data, predictors)
+data.prepare(predictors, "Dead", invert_predictors)
+
+threshold = 0.04
+num_combined_features = 30
+
+num_splits = 20
+import csv
+csvfile = open('splits.csv', 'w', newline='')
+csvwriter = csv.writer(csvfile, delimiter=';')
+csvwriter.writerow(["auc1", "sen1", "spec1", "auc2", "sen2", "spec2", "auc3", "sen3", "spec3"])
+for it in range(1, 1 + num_splits):
+    print("SPLIT #", it, "of", num_splits)
+    x_train, x_test, y_train, y_test = \
+        train_test_split(data.x, data.y, test_size=0.2, stratify=data.y)  #, random_state=123)  # закомментировать random_state
+
+    model = InitialMaxAUCModel()
+    #
+    #model = CombinedMaxAUCModel(verbose_training=True, K=num_combined_features,
+    #                            individual_training_iterations=25,
+    #                            combined_training_iterations=10)
+#    model = NewCombinedFeaturesModel(verbose_training=False, p0=threshold,
+#        K=num_combined_features, delta_a=0.2, delta_w=0.3,
+#        individual_training_iterations=25, combined_training_iterations=10)
+    model.fit(x_train, y_train)
+    #model.fit_intercept(x_train, y_train)
+
+    print_model(model, data)
+    test_model(model, x_test, y_test, threshold)
+
+"""
+    # проверяем модель
+    print("Проверка модели 1")
+    model1 = LogisticRegression()
+    model1.fit(x_train, y_train)
+    auc1, sen1, spec1 = test_model(model1, x_test, y_test, threshold)
+
+    # проверяем модель
+    print("Проверка модели 2")
+    ind_model = IndividualMaxAUCModel(verbose_training=False,
+        training_iterations=25)
+    ind_model.fit(x_train, y_train)
+    ind_model.fit_intercept(x_train, y_train)
+    #test_model(ind_model, x_test, y_test, threshold)
+    bin_x_train = ind_model.dichotomize(x_train)
+    bin_x_test = ind_model.dichotomize(x_test)
+    model2 = LogisticRegression()
+    model2.fit(bin_x_train, y_train)
+    #print("Веса =", model2.coef_)
+    auc2, sen2, spec2 = test_model(model2, bin_x_test, y_test, threshold)
+
+    # проверяем модель
+    print("Проверка модели 3")
+    bin_x_train_combined = model.dichotomize_combined(x_train)
+    bin_x_test_combined = model.dichotomize_combined(x_test)
+    model3 = LogisticRegression()
+    model3.fit(bin_x_train_combined, y_train)
+    #print("Веса =", model3.coef_)
+    auc3, sen3, spec3 = test_model(model3, bin_x_test_combined, y_test, threshold)
+
+    csvwriter.writerow(map(str, [auc1, sen1, spec1, auc2, sen2, spec2, auc3, sen3, spec3]))
+"""
---
+++ max_auc/max_auc_model.py
@@ -0,0 +1,83 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_curve, auc
+from dichotomization.calc_functions import stable_sigmoid
+
+
+class InitialMaxAUCModel:
+    def __init__(self):
+        self.cutoffs = None
+        self.weights = None
+        self.intercept = None
+
+    def fit(self, x, y):
+        data_size, num_features = x.shape[0], x.shape[1]
+        self.cutoffs = np.zeros(num_features)
+        # находим пороги в отдельности для каждого k-го признака
+        for k in range(num_features):
+            # разбиваем диапазон значений k-го признака на 100 частей
+            grid = np.linspace(np.min(x[:, k]), np.max(x[:, k]), 100, endpoint=False)
+            max_auc = None
+            optimal_cutoff = None
+            for cutoff in grid:
+                # бинаризуем данные с выбранным порогом
+                bin_x = np.where(x[:, k] >= cutoff, 1, 0).reshape(-1, 1)
+                # обучаем модель логистической регрессии на бинаризованных данных
+                model = LogisticRegression(solver='lbfgs', max_iter=10000)
+                model.fit(bin_x, y)
+                y_pred_log = model.predict_proba(bin_x)[:, 1]
+                # находим AUC для построенной модели
+                fpr, tpr, _ = roc_curve(y, y_pred_log)
+                roc_auc = auc(fpr, tpr)
+                if max_auc is None or roc_auc > max_auc:
+                    max_auc = roc_auc
+                    optimal_cutoff = cutoff
+            print("k =", k, " cutoff =", optimal_cutoff)
+            self.cutoffs[k] = optimal_cutoff
+
+        # далее обучим модель логистической регрессии на данных, бинаризованных
+        #   с найденными порогами, чтобы найти весовые коэффициенты и интерсепт
+
+        # бинаризуем данные
+        bin_x = self.dichotomize(x)
+
+        # обучаем логистическую регрессию на бинарных данных
+        logist_reg = LogisticRegression()
+        logist_reg.fit(bin_x, y)
+
+        self.weights = logist_reg.coef_.ravel()
+        self.intercept = logist_reg.intercept_[0]
+
+
+    def dichotomize(self, x):
+        data_size, num_features = x.shape[0], x.shape[1]
+        bin_x = np.empty_like(x, dtype=int)
+        for k in range(num_features):
+            bin_x[:, k] = np.where(x[:, k] >= self.cutoffs[k], 1, 0)
+        return bin_x
+
+
+    def predict_proba(self, x):
+        bin_x = self.dichotomize(x)
+        z = np.dot(bin_x, self.weights) + self.intercept
+        probs = np.array([stable_sigmoid(value) for value in z])
+        return np.c_[1 - probs, probs]
+
+
+
+
+
+
+
+
+
+class IndividualMaxAUCModel:
+    def __init__(self, verbose_training=False, training_iterations=20):
+        self.cutoffs = None
+        self.weights = None
+        self.intercept = None
+
+        self.verbose_training = verbose_training
+        self.training_iterations = training_iterations
+
+   # def fit(self, x, y):

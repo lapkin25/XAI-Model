6be6d4a7db97cbb3cb6235daaf9f648211ebde9b
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2024-05-20 17:03:00 +1000

    Remove first combined features

--- max_auc/max_auc_model.py
+++ max_auc/max_auc_model.py
@@ -172,7 +172,6 @@ class CombinedMaxAUCModel:
         self.K = K
 
     def fit(self, x, y):
-        data_size, num_features = x.shape[0], x.shape[1]
         self.cutoffs = self.ind_model.cutoffs
         self.individual_weights = self.ind_model.weights
         self.intercept = self.ind_model.intercept
@@ -182,66 +181,88 @@ class CombinedMaxAUCModel:
         for it in range(self.K):
             # добавляем дополнительный комбинированный признак
             # для этого выбираем его из условия максимума AUC
-            bin_x = self.dichotomize_combined(x)
-            best_auc = 0.0
-            best_k = None
-            best_j = None
-            best_xj_cutoff = None
-            for k in range(num_features):
-                bin_xk = np.where(x[:, k] >= self.cutoffs[k], 1, 0).reshape(-1, 1)
-                # пробуем добавить к k-му признаку какой-нибудь j-й,
-                # чтобы максимизировать AUC полученной модели с бинарными признаками
-                for j in range(num_features):
-                    if j == k:
-                        continue
-                    # проверяем, добавлялось ли такое сочетание признаков
-                    ok = True
-                    for k1, j1, _ in self.combined_features:
-                        if k1 == k and j1 == j:
-                            ok = False
-                    if not ok:
-                        continue
+            self.add_combined_feature(x, y, it)
 
-                    # взвешенная сумма для остальных признаков
-                    rest = np.array([np.dot(self.combined_weights, bin_x[i]) for i in range(data_size)]).reshape(-1, 1)
-                    # сетка для перебора порогов
-                    grid = np.linspace(np.min(x[:, j]), np.max(x[:, j]), 100, endpoint=False)
-                    max_auc = None
-                    optimal_cutoff = None
-                    for cutoff in grid:
-                        # бинаризуем данные с выбранным порогом
-                        bin_xj = np.where(x[:, j] >= cutoff, 1, 0).reshape(-1, 1)
-                        # обучаем модель логистической регрессии на бинаризованных данных
-                        log_reg = LogisticRegression(solver='lbfgs', max_iter=10000)
-                        # два признака: взвешенная сумма остальных признаков и бинарный признак x_k & x_j
-                        new_x = np.c_[rest, bin_xk * bin_xj]
-                        log_reg.fit(new_x, y)
-                        y_pred_log = log_reg.predict_proba(new_x)[:, 1]
-                        # находим AUC для построенной модели
-                        fpr, tpr, _ = roc_curve(y, y_pred_log)
-                        roc_auc = auc(fpr, tpr)
-                        if max_auc is None or roc_auc > max_auc:
-                            max_auc = roc_auc
-                            optimal_cutoff = cutoff
-                    # print("k =", k, " j =", j, "xj_cutoff =", optimal_cutoff, "auc =", max_auc)
-                    if max_auc > best_auc:
-                        best_auc = max_auc
-                        best_j = j
-                        best_k = k
-                        best_xj_cutoff = optimal_cutoff
+        # удаляем первые 5 комбинированных признаков
+        for it in range(5):
+            self.combined_features.pop(0)
+            self.combined_weights.pop(0)
 
-            # добавляем новую комбинацию признаков
-            if self.verbose_training:
-                print("Новая комбинация №", it + 1, ": k =", best_k, ", j =", best_j,
-                      ", порог для xj =", best_xj_cutoff, ", AUC =", best_auc)
-            self.combined_features.append((best_k, best_j, best_xj_cutoff))
+        # настраиваем все веса с найденными порогами
+        bin_x = self.dichotomize_combined(x)
+        logist_reg = LogisticRegression()
+        logist_reg.fit(bin_x, y)
+        self.combined_weights = logist_reg.coef_.ravel()
+        self.intercept = logist_reg.intercept_[0]
 
-            # настраиваем все веса с найденными порогами
-            bin_x = self.dichotomize_combined(x)
-            logist_reg = LogisticRegression()
-            logist_reg.fit(bin_x, y)
-            self.combined_weights = logist_reg.coef_.ravel()
-            self.intercept = logist_reg.intercept_[0]
+        # добавляем 5 признаков заново
+        for it in range(5):
+            # добавляем дополнительный комбинированный признак
+            # для этого выбираем его из условия максимума AUC
+            self.add_combined_feature(x, y, it)
+
+    def add_combined_feature(self, x, y, it):
+        data_size, num_features = x.shape[0], x.shape[1]
+        bin_x = self.dichotomize_combined(x)
+        best_auc = 0.0
+        best_k = None
+        best_j = None
+        best_xj_cutoff = None
+        for k in range(num_features):
+            bin_xk = np.where(x[:, k] >= self.cutoffs[k], 1, 0).reshape(-1, 1)
+            # пробуем добавить к k-му признаку какой-нибудь j-й,
+            # чтобы максимизировать AUC полученной модели с бинарными признаками
+            for j in range(num_features):
+                if j == k:
+                    continue
+                # проверяем, добавлялось ли такое сочетание признаков
+                ok = True
+                for k1, j1, _ in self.combined_features:
+                    if k1 == k and j1 == j:
+                        ok = False
+                if not ok:
+                    continue
+
+                # взвешенная сумма для остальных признаков
+                rest = np.array([np.dot(self.combined_weights, bin_x[i]) for i in range(data_size)]).reshape(-1, 1)
+                # сетка для перебора порогов
+                grid = np.linspace(np.min(x[:, j]), np.max(x[:, j]), 100, endpoint=False)
+                max_auc = None
+                optimal_cutoff = None
+                for cutoff in grid:
+                    # бинаризуем данные с выбранным порогом
+                    bin_xj = np.where(x[:, j] >= cutoff, 1, 0).reshape(-1, 1)
+                    # обучаем модель логистической регрессии на бинаризованных данных
+                    log_reg = LogisticRegression(solver='lbfgs', max_iter=10000)
+                    # два признака: взвешенная сумма остальных признаков и бинарный признак x_k & x_j
+                    new_x = np.c_[rest, bin_xk * bin_xj]
+                    log_reg.fit(new_x, y)
+                    y_pred_log = log_reg.predict_proba(new_x)[:, 1]
+                    # находим AUC для построенной модели
+                    fpr, tpr, _ = roc_curve(y, y_pred_log)
+                    roc_auc = auc(fpr, tpr)
+                    if max_auc is None or roc_auc > max_auc:
+                        max_auc = roc_auc
+                        optimal_cutoff = cutoff
+                # print("k =", k, " j =", j, "xj_cutoff =", optimal_cutoff, "auc =", max_auc)
+                if max_auc > best_auc:
+                    best_auc = max_auc
+                    best_j = j
+                    best_k = k
+                    best_xj_cutoff = optimal_cutoff
+
+        # добавляем новую комбинацию признаков
+        if self.verbose_training:
+            print("Новая комбинация №", it + 1, ": k =", best_k, ", j =", best_j,
+                  ", порог для xj =", best_xj_cutoff, ", AUC =", best_auc)
+        self.combined_features.append((best_k, best_j, best_xj_cutoff))
+
+        # настраиваем все веса с найденными порогами
+        bin_x = self.dichotomize_combined(x)
+        logist_reg = LogisticRegression()
+        logist_reg.fit(bin_x, y)
+        self.combined_weights = logist_reg.coef_.ravel()
+        self.intercept = logist_reg.intercept_[0]
 
     def dichotomize_combined(self, x):
         data_size, num_features = x.shape[0], x.shape[1]

836ccca2349bcd9cd950000840b64b5901bd4965
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2024-07-02 20:58:03 +1000

    Update main_max_auc.py

--- max_auc/main_max_auc.py
+++ max_auc/main_max_auc.py
@@ -109,10 +109,10 @@ predictors = ["Age", "HR", "Killip class", "Cr", "EF LV", "NEUT", "EOS", "PCT", 
 invert_predictors = find_predictors_to_invert(data, predictors)
 data.prepare(predictors, "Dead", invert_predictors)
 
-threshold = 0.04
+threshold = 0.03  #0.04
 num_combined_features = 10
 
-num_splits = 1
+num_splits = 30
 
 csvfile = open('splits.csv', 'w', newline='')
 csvwriter = csv.writer(csvfile, delimiter=';')
@@ -122,7 +122,7 @@ csvwriter.writerow(["auc0", "sen0", "spec0", "auc1", "sen1", "spec1", "auc2", "s
 for it in range(1, 1 + num_splits):
     print("SPLIT #", it, "of", num_splits)
     x_train, x_test, y_train, y_test = \
-        train_test_split(data.x, data.y, test_size=0.2, stratify=data.y, random_state=123)  # закомментировать random_state
+        train_test_split(data.x, data.y, test_size=0.2, stratify=data.y)  #, random_state=123)  # закомментировать random_state
 
     initial_model = InitialMaxAUCModel()
     initial_model.fit(x_train, y_train)

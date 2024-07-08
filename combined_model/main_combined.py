87b91ba9c299f004627fd20d71c940d6b7576b03
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2024-07-08 18:48:53 +1000

    Update main_combined.py

--- combined_model/main_combined.py
+++ combined_model/main_combined.py
@@ -9,6 +9,7 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn import metrics as sklearn_metrics
+import csv
 
 
 def find_predictors_to_invert(data, predictors):
@@ -25,41 +26,24 @@ def find_predictors_to_invert(data, predictors):
     return invert_predictors
 
 
-"""
-def print_model(model, data):
+def print_combined_model(model, data):
     print("=" * 10 + "\nМодель")
     print("Пороги:")
     for k, feature_name in enumerate(predictors):
         val = data.get_coord(feature_name, model.cutoffs[k])
         s = '≤' if feature_name in data.inverted_predictors else '≥'
         print(feature_name, " ", s, val, sep='')
-    print("Комбинированные пороги:")
-    for k, j, xj_cutoff in model.combined_features:
-        feature_name = predictors[k]
-        val = data.get_coord(feature_name, model.cutoffs[k])
-        s = '≤' if feature_name in data.inverted_predictors else '≥'
-        print(feature_name, " ", s, val, sep='', end='')
-        feature_name = predictors[j]
-        val = data.get_coord(feature_name, xj_cutoff)
-        s = '≤' if feature_name in data.inverted_predictors else '≥'
-        print(" & ", feature_name, " ", s, val, sep='')
-        tp = 0
-        fp = 0
-        y_pred = np.zeros(data.x.shape[0])
-        for i in range(data.x.shape[0]):
-            if data.x[i, k] >= model.cutoffs[k] and data.x[i, j] >= xj_cutoff:
-                y_pred[i] = 1
-                if data.y[i] == 1:
-                    tp += 1
-                else:
-                    fp += 1
-        print("   TP = ", tp, " FP = ", fp, " Prob = ", tp / (tp + fp))
-        cm = ClassificationMetric(data.y, y_pred)
-        print("   Gini = ", cm.gini_index(average=None))
-    print("Веса:", model.individual_weights)
-    print("Комбинированные веса:", model.combined_weights)
+    print("Веса:", model.weights)
     print("Интерсепт:", model.intercept)
-"""
+    for k in range(len(predictors)):
+        print("Вспомогательная модель для признака", predictors[k])
+        for j in range(len(predictors)):
+            feature_name = predictors[j]
+            val = data.get_coord(feature_name, model.middle_cutoffs[k, j])
+            s = '≤' if feature_name in data.inverted_predictors else '≥'
+            print("  ", feature_name, " ", s, val, sep='')
+        print("Веса:", model.middle_weights[k, :])
+        print("Интерсепт:", model.middle_intercept[k])
 
 
 def test_model(model, x_test, y_test, p_threshold):
@@ -83,20 +67,31 @@ data.prepare(predictors, "Dead", invert_predictors)
 
 threshold = 0.03
 
-x_train, x_test, y_train, y_test = \
-    train_test_split(data.x, data.y, test_size=0.2, stratify=data.y, random_state=123)  # закомментировать random_state
+num_splits = 30
 
-print("Начальная модель")
-initial_model = InitialMaxAUCModel()
-initial_model.fit(x_train, y_train)
-auc0, sen0, spec0 = test_model(initial_model, x_test, y_test, threshold)
+csvfile = open('splits.csv', 'w', newline='')
+csvwriter = csv.writer(csvfile, delimiter=';')
+csvwriter.writerow(["auc0", "sen0", "spec0", "auc1", "sen1", "spec1", "auc2", "sen2", "spec2"])
 
-print("Непрерывная модель")
-continuous_model = LogisticRegression()
-continuous_model.fit(x_train, y_train)
-auc1, sen1, spec1 = test_model(continuous_model, x_test, y_test, threshold)
+for it in range(1, 1 + num_splits):
+    print("SPLIT #", it, "of", num_splits)
+    x_train, x_test, y_train, y_test = \
+        train_test_split(data.x, data.y, test_size=0.2, stratify=data.y)  #, random_state=123)  # закомментировать random_state
 
-print("Комбинированная модель")
-combined_model = CombinedModel(initial_model, threshold=threshold)
-combined_model.fit(x_train, y_train)
-auc2, sen2, spec2 = test_model(combined_model, x_test, y_test, threshold)
+    print("Начальная модель")
+    initial_model = InitialMaxAUCModel()
+    initial_model.fit(x_train, y_train)
+    auc0, sen0, spec0 = test_model(initial_model, x_test, y_test, threshold)
+
+    print("Непрерывная модель")
+    continuous_model = LogisticRegression()
+    continuous_model.fit(x_train, y_train)
+    auc1, sen1, spec1 = test_model(continuous_model, x_test, y_test, threshold)
+
+    print("Комбинированная модель")
+    combined_model = CombinedModel(initial_model, threshold=threshold)
+    combined_model.fit(x_train, y_train)
+    print_combined_model(combined_model, data)
+    auc2, sen2, spec2 = test_model(combined_model, x_test, y_test, threshold)
+
+    csvwriter.writerow(map(str, [auc0, sen0, spec0, auc1, sen1, spec1, auc2, sen2, spec2]))

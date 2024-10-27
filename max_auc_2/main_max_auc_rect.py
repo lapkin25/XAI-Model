439a9bf6cbc5d4801ca09a607ecaba5e10b64203
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2024-10-27 13:40:53 +1000

    Model with feature selection

--- max_auc_2/main_max_auc_rect.py
+++ max_auc_2/main_max_auc_rect.py
@@ -3,9 +3,12 @@ sys.path.insert(1, '../dichotomization')
 
 from dichotomization.read_data import Data
 from sklearn.linear_model import LogisticRegression
+from sklearn import metrics as sklearn_metrics
+from sklearn.model_selection import train_test_split
 import numpy as np
 from sortedcontainers import SortedList
 import matplotlib.pyplot as plt
+import csv
 
 
 def find_predictors_to_invert(data, predictors):
@@ -76,11 +79,105 @@ def plot_2d(x1, x1_name, x2, x2_name, y, a, b):
     plt.show()
 
 
+class MaxAUCRectModel:
+    def __init__(self, num_combined_features):
+        self.num_combined_features = num_combined_features
+        self.features_used = None
+        self.model = None
+        self.thresholds = None
+
+    def fit(self, x, y):
+        data_size, num_features = x.shape[0], x.shape[1]
+        z = None
+        self.thresholds = []
+        for ind1 in range(num_features):
+            for ind2 in range(ind1 + 1, num_features):
+                print("ind = ", ind1, "," , ind2)
+                a, b, auc1 = find_threshold_rect(x[:, ind1], x[:, ind2], y[:])
+                print(a, b)
+                print("AUC =", auc1)
+                row = np.zeros(data_size, dtype=int)
+                for i in range(data_size):
+                    if x[i, ind1] >= a and x[i, ind2] >= b:
+                        row[i] = 1
+                if z is None:
+                    z = row
+                else:
+                    z = np.vstack((z, row))
+                self.thresholds.append({'a': a, 'b': b, 'auc': auc1})
+
+        z = z.T
+        print(z)
+        print(self.thresholds)
+
+        # Отбор признаков методом включения
+        max_auc = 0.0
+        best_feature = None
+        num_pair_features = z.shape[1]
+        for feature in range(num_pair_features):
+            print("Признак", feature + 1)
+            model = LogisticRegression(solver='lbfgs', max_iter=10000)
+            model.fit(z[:, feature].reshape(-1, 1), y)
+            p = model.predict_proba(z[:, feature].reshape(-1, 1))[:, 1]
+            auc = sklearn_metrics.roc_auc_score(y, p)
+            if auc > max_auc:
+                max_auc = auc
+                best_feature = feature
+        #print(best_feature, max_auc)
+
+        features_used = [best_feature]
+
+        for feature_cnt in range(1, self.num_combined_features):
+            max_auc = 0.0
+            best_feature = None
+            for feature in range(num_pair_features):
+                if feature not in features_used:
+                    features_used.append(feature)
+                    model = LogisticRegression(solver='lbfgs', max_iter=10000)
+                    model.fit(z[:, features_used], y)
+                    p = model.predict_proba(z[:, features_used])[:, 1]
+                    auc = sklearn_metrics.roc_auc_score(y, p)
+                    if auc > max_auc:
+                        max_auc = auc
+                        best_feature = feature
+                    features_used.pop()
+            features_used.append(best_feature)
+            print("AUC =", max_auc)
+
+        model = LogisticRegression(solver='lbfgs', max_iter=10000)
+        model.fit(z[:, features_used], y)
+        self.model = model
+        self.features_used = features_used
+
+    def predict_proba(self, x):
+        data_size, num_features = x.shape[0], x.shape[1]
+        z = None
+        k = 0
+        for ind1 in range(num_features):
+            for ind2 in range(ind1 + 1, num_features):
+                row = np.zeros(data_size, dtype=int)
+                a = self.thresholds[k]['a']
+                b = self.thresholds[k]['b']
+                for i in range(data_size):
+                    if x[i, ind1] >= a and x[i, ind2] >= b:
+                        row[i] = 1
+                if z is None:
+                    z = row
+                else:
+                    z = np.vstack((z, row))
+                k += 1
+
+        z = z.T
+
+        return self.model.predict_proba(z[:, self.features_used])
+
+
 data = Data("DataSet.xlsx")
 predictors = ["Age", "HR", "Killip class", "Cr", "EF LV", "NEUT", "EOS", "PCT", "Glu", "SBP"]
 invert_predictors = find_predictors_to_invert(data, predictors)
 data.prepare(predictors, "Dead", invert_predictors)
 
+"""
 ind1 = 0
 ind2 = 5
 
@@ -88,3 +185,53 @@ a, b, auc = find_threshold_rect(data.x[:, ind1], data.x[:, ind2], data.y[:])
 print(a, b)
 print("AUC =", auc)
 plot_2d(data.x[:, ind1], predictors[ind1], data.x[:, ind2], predictors[ind2], data.y[:], a, b)
+"""
+
+threshold = 0.03  #0.04
+num_combined_features = 12  #10
+
+num_splits = 1  # 30
+random_state = 123
+
+csvfile = open('splits.csv', 'w', newline='')
+csvwriter = csv.writer(csvfile, delimiter=';')
+csvwriter.writerow(["auc", "sen", "spec"])
+
+for it in range(1, 1 + num_splits):
+    print("SPLIT #", it, "of", num_splits)
+
+    x_train, x_test, y_train, y_test = \
+        train_test_split(data.x, data.y, test_size=0.2, stratify=data.y, random_state=random_state)  # закомментировать random_state
+
+    max_auc_rect_model = MaxAUCRectModel(num_combined_features)
+    max_auc_rect_model.fit(x_train, y_train)
+
+    print("Обучена модель")
+    data_size, num_features = data.x.shape[0], data.x.shape[1]
+    #print(max_auc_2_model.model.coef_.ravel())
+
+    k = 0
+    for ind1 in range(num_features):
+        for ind2 in range(ind1 + 1, num_features):
+            if k in max_auc_rect_model.features_used:
+                print(ind1, ind2, max_auc_rect_model.model.coef_[0][max_auc_rect_model.features_used.index(k)])
+                print(max_auc_rect_model.thresholds[k])
+                a = max_auc_rect_model.thresholds[k]['a']
+                b = max_auc_rect_model.thresholds[k]['b']
+                print("  AUC =", max_auc_rect_model.thresholds[k]['auc'])
+                plot_2d(data.x[:, ind1], predictors[ind1], data.x[:, ind2], predictors[ind2], data.y[:], a, b)
+            k += 1
+
+
+    p = max_auc_rect_model.predict_proba(x_test)[:, 1]
+    auc = sklearn_metrics.roc_auc_score(y_test, p)
+    print("AUC:", auc)
+    # выводим качество модели
+    y_pred = np.where(p > threshold, 1, 0)
+    tn, fp, fn, tp = sklearn_metrics.confusion_matrix(y_test, y_pred).ravel()
+    specificity = tn / (tn + fp)
+    sensitivity = tp / (tp + fn)
+    print("Sens:", sensitivity, "Spec:", specificity)
+    print("tp =", tp, "fn =", fn, "fp =", fp, "tn =", tn)
+
+    csvwriter.writerow(map(str, [auc, sensitivity, specificity]))

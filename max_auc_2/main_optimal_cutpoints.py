adf4cf2b64cad21c5cc35df8604deebf955633dd
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2025-07-11 07:26:16 +1000

    Библиотека PyGAD

--- max_auc_2/main_optimal_cutpoints.py
+++ max_auc_2/main_optimal_cutpoints.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split, cross_validate, Stratified
 from sklearn.metrics import make_scorer, confusion_matrix, roc_auc_score, f1_score, accuracy_score, recall_score
 from geneticalgorithm import geneticalgorithm as ga
 import sklearn.metrics as sklearn_metrics
+import pygad
 
 
 class LogisticPairsModel:
@@ -123,10 +124,46 @@ class PairsModel:
             #return -calc_J(c)
             return -val
 
+        """
         model = ga(function=f, dimension=num_features, variable_type='real', variable_boundaries=varbound,
                    algorithm_parameters=algorithm_param, convergence_curve=True)  # False)
-        model.run()
+        model.run()        
         self.cutpoints = model.output_dict['variable']
+        """
+
+        def fitness_func(ga_instance, solution, solution_idx):
+            return calc_J(solution)
+
+        num_generations = 100  # Number of generations.
+        num_parents_mating = 10  # Number of solutions to be selected as parents in the mating pool.
+
+        sol_per_pop = 20  # Number of solutions in the population.
+        num_genes = num_features
+
+        def on_generation(ga_instance):
+            print(f"Generation = {ga_instance.generations_completed}")
+            print(f"Fitness    = {ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1]}")
+
+        ga_instance = pygad.GA(num_generations=num_generations,
+                               num_parents_mating=num_parents_mating,
+                               sol_per_pop=sol_per_pop,
+                               num_genes=num_genes,
+                               fitness_func=fitness_func,
+                               on_generation=on_generation)
+
+        # Running the GA to optimize the parameters of the function.
+        ga_instance.run()
+
+        ga_instance.plot_fitness()
+
+        # Returning the details of the best solution.
+        solution, solution_fitness, solution_idx = ga_instance.best_solution(ga_instance.last_generation_fitness)
+        print(f"Parameters of the best solution : {solution}")
+        print(f"Fitness value of the best solution = {solution_fitness}")
+        print(f"Index of the best solution : {solution_idx}")
+
+        self.cutpoints = solution
+
         # далее обучить логистическую регрессию на парах
         z = np.zeros((data_size, num_features), dtype=int)
         for j in range(num_features):

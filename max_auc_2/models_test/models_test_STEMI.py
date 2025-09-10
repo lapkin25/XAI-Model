7a5481bbd97aa50f567a0544e34e81b9fafde9f9
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2025-09-10 16:26:27 +1000

    Пересчет 1

--- max_auc_2/models_test/models_test_STEMI.py
+++ max_auc_2/models_test/models_test_STEMI.py
@@ -58,7 +58,7 @@ def custom_specificity_score(y_true, proba, threshold=threshold):
 data = Data("STEMI.xlsx", STEMI=True)
 
 predictors = ['Возраст', 'NER1', 'SIRI', 'СОЭ', 'TIMI после', 'СДЛА', 'Killip',
-              'RR 600-1200', 'интервал PQ 120-200']
+              'RR 600-1200', 'интервал PQ 120-200', 'NBR1', 'SII']
 
 data.prepare(predictors, "isAFAfter", [], scale_data=False)
 
@@ -70,21 +70,58 @@ for i, name in enumerate(predictors):
 df.loc[:, 'isAFAfter'] = data.y[:]
 
 
-isModel = 3  # 1 - Logistic
+# SIRI ≥ -0.6850634569281044 * Возраст + 47.74373157512894
+df.loc[(df['SIRI'] >= -0.6850634569281044 * df['Возраст'] + 47.74373157512894), ('F1')] = 1
+df.loc[(df['SIRI'] < -0.6850634569281044 * df['Возраст'] + 47.74373157512894), ('F1')] = 0
+
+# Killip ≥ -0.2760425151659211 * Возраст + 19.047800827844767
+df.loc[(df['Killip'] >= -0.2760425151659211 * df['Возраст'] + 19.047800827844767), ('F2')] = 1
+df.loc[(df['Killip'] < -0.2760425151659211 * df['Возраст'] + 19.047800827844767), ('F2')] = 0
+
+# СОЭ ≥ -0.8588676342460239 * NER1 + 86.15188317318132
+df.loc[(df['СОЭ'] >= -0.8588676342460239 * df['NER1'] + 86.15188317318132), ('F3')] = 1
+df.loc[(df['СОЭ'] < -0.8588676342460239 * df['NER1'] + 86.15188317318132), ('F3')] = 0
+
+# интервал PQ 120-200 ≥ -3.4850431712105663 * NER1 + 408.90755833216843
+df.loc[(df['интервал PQ 120-200'] >= -3.4850431712105663 * df['NER1'] + 408.90755833216843), ('F4')] = 1
+df.loc[(df['интервал PQ 120-200'] < -3.4850431712105663 * df['NER1'] + 408.90755833216843), ('F4')] = 0
+
+# СДЛА ≥ -2.0332049131752137 * SIRI + 39.34501349469828
+df.loc[(df['СДЛА'] >= -2.0332049131752137 * df['SIRI'] + 39.34501349469828), ('F5')] = 1
+df.loc[(df['СДЛА'] < -2.0332049131752137 * df['SIRI'] + 39.34501349469828), ('F5')] = 0
+
+# TIMI после ≤ 0.06936743913420756 * СОЭ + 0.3475248603310528
+df.loc[(df['TIMI после'] <= 0.06936743913420756 * df['СОЭ'] + 0.3475248603310528), ('F6')] = 1
+df.loc[(df['TIMI после'] > 0.06936743913420756 * df['СОЭ'] + 0.3475248603310528), ('F6')] = 0
+
+# Killip ≥ -0.10081756948866635 * СОЭ + 4.221636904507594
+df.loc[(df['Killip'] >= -0.10081756948866635 * df['СОЭ'] + 4.221636904507594), ('F7')] = 1
+df.loc[(df['Killip'] < -0.10081756948866635 * df['СОЭ'] + 4.221636904507594), ('F7')] = 0
+
+# интервал PQ 120-200 ≥ 3.9012993788600223 * TIMI после + 150.00224513610706
+df.loc[(df['интервал PQ 120-200'] >= 3.9012993788600223 * df['TIMI после'] + 150.00224513610706), ('F8')] = 1
+df.loc[(df['интервал PQ 120-200'] < 3.9012993788600223 * df['TIMI после'] + 150.00224513610706), ('F8')] = 0
+
+# RR 600-1200 ≤ 644.1891477774482 * Killip + -228.24369469593375
+df.loc[(df['RR 600-1200'] <= 644.1891477774482 * df['Killip'] - 228.24369469593375), ('F9')] = 1
+df.loc[(df['RR 600-1200'] > 644.1891477774482 * df['Killip'] - 228.24369469593375), ('F9')] = 0
+
+
+
+features = [ 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9' ]
+
+isModel = 1 # 1 - Logistic
 rm = 100
-# border = 0.07    - logistic
-# border = 0.08   - CatBoost
-# border = 0.085   - RandomForest
-border = 0.08
+border = 0.09 #0.08
 np.random.seed(rm)
 
-x_all = np.array(df[predictors], dtype=int)
+x_all = np.array(df[features], dtype=int)
 y1 = np.array(df['isAFAfter'].astype('int'))
 #y_all = utils.to_categorical(y1)
 
 # Параметры для модели
 solver1 = 'lbfgs'
-max_iter1 = 10000
+max_iter1 = 2000
 C1 = 1
 penalty1 = 'l2'
 
@@ -95,6 +132,121 @@ spw = 1
 
 m_d1 = 3
 n_e1 = 180
+
+# оцениваем точность каждого фактора риска
+for feat in features:
+    print("ФР", feat)
+
+    # Для хранения результатов
+    mean_roc_auc_test = []
+    mean_sen_test = []
+    mean_spec_test = []
+    mean_f1_test = []
+    mean_acc_test = []
+    mean_ppv_test = []
+    mean_npv_test = []
+
+    mean_roc_auc = []
+    mean_sensitivity = []
+    mean_specificity = []
+    mean_acc = []
+    mean_f1 = []
+    mean_ppv = []
+    mean_npv = []
+
+    # Выборка из одного признака
+    x_feat = np.array(df[feat]).reshape(-1, 1)
+
+    ct = pd.crosstab(y1, df[feat])
+    table = sm.stats.Table2x2(ct, shift_zeros=False)
+    print(table)
+    odds_ratio = table.oddsratio
+    confint = table.oddsratio_confint()
+
+    print('Odds ratio, 95% CI', odds_ratio, confint)
+
+    # Повторить 10 раз
+    for j in range(100):
+        #print("SPLIT #", j)
+        np.random.seed(j + 42)
+
+        # Случайное разделение данных на обучающую (80%) и тестовую (20%) выборки
+        x_train, x_validate, y_train, y_validate = train_test_split(x_feat, y1, train_size=0.8, stratify=y1,
+                                                                    random_state=j + 42)
+        model = LogisticRegression(solver=solver1, max_iter=max_iter1, C=C1, penalty=penalty1)
+        # Настроим метрики для кросс-валидации
+        scoring = {'roc_auc': make_scorer(roc_auc_score, response_method='predict_proba'),
+                   'f1': make_scorer(custom_f1_score, response_method='predict_proba', threshold=border),
+                   'accuracy': make_scorer(custom_accuracy_score, response_method='predict_proba', threshold=border),
+                   'sensitivity': make_scorer(custom_recall_score, response_method='predict_proba', threshold=border),
+                   'specificity': make_scorer(custom_specificity_score, response_method='predict_proba', threshold=border)
+                   }
+        # Выполним кросс-валидацию с использованием cross_validate
+        cv_results = cross_validate(model, x_train, y_train, cv=StratifiedKFold(n_splits=10),
+                                    scoring=scoring, return_train_score=False)
+        # Сохранение метрик кросс-валидации
+        mean_roc_auc.append(np.mean(cv_results['test_roc_auc']))
+        mean_f1.append(np.mean(cv_results['test_f1']))
+        mean_acc.append(np.mean(cv_results['test_accuracy']))
+        mean_sensitivity.append(np.mean(cv_results['test_sensitivity']))
+        mean_specificity.append(np.mean(cv_results['test_specificity']))
+
+        # Обучение модели на всей обучающей выборке
+        model.fit(x_train, y_train)
+        # Тестирование на валидационной выборке (20%)
+        y_pred_prob = model.predict_proba(x_validate)[:, 1]  # Вероятности для положительного класса
+        y_pred = (y_pred_prob >= border).astype(int)
+        # Матрица ошибок
+        cMatrix = confusion_matrix(y_validate, y_pred)
+        sensivity = cMatrix[1][1] / (cMatrix[1][0] + cMatrix[1][1])
+        specifity = cMatrix[0][0] / (cMatrix[0][0] + cMatrix[0][1])
+        fpr, tpr, _ = roc_curve(y_validate, y_pred_prob)
+        roc_auc = auc(fpr, tpr)
+        f1 = f1_score(y_validate, y_pred)
+        acc = accuracy_score(y_validate, y_pred)
+        ppv = cMatrix[1][1] / (cMatrix[1][1] + cMatrix[0][1])
+        npv = cMatrix[0][0] / (cMatrix[0][0] + cMatrix[1][0])
+
+        # Сохранение результатов
+        mean_roc_auc_test.append(roc_auc)
+        mean_sen_test.append(sensivity)
+        mean_spec_test.append(specifity)
+        mean_f1_test.append(f1)
+        mean_acc_test.append(acc)
+        mean_ppv_test.append(ppv)
+        mean_npv_test.append(npv)
+
+    roc_auc_mean, roc_auc_lower, roc_auc_upper = compute_confidence_interval(mean_roc_auc)
+    f1_mean, f1_lower, f1_upper = compute_confidence_interval(mean_f1)
+    acc_mean, acc_lower, acc_upper = compute_confidence_interval(mean_acc)
+    sen_mean, sen_lower, sen_upper = compute_confidence_interval(mean_sensitivity)
+    spec_mean, spec_lower, spec_upper = compute_confidence_interval(mean_specificity)
+
+    # Вывод результатов
+    print(f"Кросс-валидационный ROC-AUC: {roc_auc_mean:.4f} 95% [ {roc_auc_lower:.4f}, {roc_auc_upper:.4f} ]")
+    #print(f"Кросс-валидация Чувствительность: {sen_mean:.4f} 95% [ {sen_lower:.4f}, {sen_upper:.4f} ]")
+    #print(f"Кросс-валидация Специфичность: {spec_mean:.4f} 95% [ {spec_lower:.4f}, {spec_upper:.4f} ]")
+    #print(f"Кросс-валидационный F1: {f1_mean:.4f} 95% [ {f1_lower:.4f}, {f1_upper:.4f} ]")
+    #print(f"Кросс-валидационный (Accuracy): {acc_mean:.4f} 95% [ {acc_lower:.4f}, {acc_upper:.4f} ]")
+
+    # Итоговые результаты с доверительными интервалами
+    roc_auc_mean, roc_auc_lower, roc_auc_upper = compute_confidence_interval(mean_roc_auc_test)
+    sen_mean, sen_lower, sen_upper = compute_confidence_interval(mean_sen_test)
+    spec_mean, spec_lower, spec_upper = compute_confidence_interval(mean_spec_test)
+    f1_mean, f1_lower, f1_upper = compute_confidence_interval(mean_f1_test)
+    acc_mean, acc_lower, acc_upper = compute_confidence_interval(mean_acc_test)
+    ppv_mean, ppv_lower, ppv_upper = compute_confidence_interval(mean_ppv_test)
+    npv_mean, npv_lower, npv_upper = compute_confidence_interval(mean_npv_test)
+
+    # Вывод результатов
+    # print("---------------------------------------")
+    print(f"Средний ROC-AUC: {roc_auc_mean:.4f} 95% [ {roc_auc_lower:.4f}, {roc_auc_upper:.4f} ]")
+    #print(f"Средняя Чувствительность: {sen_mean:.4f} 95% [ {sen_lower:.4f}, {sen_upper:.4f} ]")
+    #print(f"Средняя Специфичность: {spec_mean:.4f} 95% [ {spec_lower:.4f}, {spec_upper:.4f} ]")
+    #print(f"Средняя F1: {f1_mean:.4f} 95% [ {f1_lower:.4f}, {f1_upper:.4f} ]")
+    #print(f"Средняя Точность (Accuracy): {acc_mean:.4f} 95% [ {acc_lower:.4f}, {acc_upper:.4f} ]")
+    #print(f"Средний PPV: {ppv_mean:.4f} 95% [ {ppv_lower:.4f}, {ppv_upper:.4f} ]")
+    #print(f"Средний NPV: {npv_mean:.4f} 95% [ {npv_lower:.4f}, {npv_upper:.4f} ]")
 
 
 
@@ -134,6 +286,7 @@ for j in range(100):
         model = CatBoostClassifier(learning_rate=lr, eval_metric="AUC", scale_pos_weight=spw, max_depth=m_d,
                                    n_estimators=n_e,
                                    random_state=j + 42,
+                                   cat_features=list(range(len(features))),  # Указываем категориальные колонки
                                    verbose=0)
     else:
         model = RandomForestClassifier(random_state=j + 42, n_estimators=n_e1, max_depth=m_d1)
